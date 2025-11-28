4ed910fea55e17a2489f94bbb3bce55ede9916c0
YoEight <yo.eight@gmail.com>
2025-11-28 14:06:13 -0500

    pending impl

--- eos/src/main.rs
+++ eos/src/main.rs
@@ -1,3 +1,52 @@
-fn main() {
-    println!("Hello, world!");
+use ratatui::{
+    Frame,
+    crossterm::event::{self, Event, KeyCode},
+};
+
+#[derive(Default)]
+struct App {
+    input: String,
+    cursor: usize,
+}
+
+impl App {
+    fn insert_char(&mut self, c: char) {
+        if self.cursor == self.input.len() - 1 {
+            self.input.push(c);
+        } else {
+            self.input.insert(self.cursor, c);
+        }
+
+        self.cursor += 1;
+    }
+
+    fn render(&self, frame: &mut Frame) {}
+}
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+    let mut terminal = ratatui::init();
+    let mut app = App::default();
+
+    loop {
+        terminal.draw(|f| app.render(f))?;
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                KeyCode::Enter => {
+                    if app.input.as_str() == ":quit" {
+                        break;
+                    }
+                }
+
+                KeyCode::Char(c) => {
+                    app.insert_char(c);
+                }
+
+                _ => {}
+            }
+        }
+    }
+
+    ratatui::restore();
+    Ok(())
 }
