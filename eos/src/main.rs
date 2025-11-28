57ee70ac064b7d3415bb8e6e64521baf01dc2812
YoEight <yo.eight@gmail.com>
2025-11-28 15:20:30 -0500

    more movements

--- eos/src/main.rs
+++ eos/src/main.rs
@@ -11,13 +11,42 @@ struct App {
 
 impl App {
     fn insert_char(&mut self, c: char) {
-        if self.cursor == self.input.len() - 1 {
+        if self.cursor >= self.input.len() {
             self.input.push(c);
+            self.cursor = self.input.len() - 1;
         } else {
             self.input.insert(self.cursor, c);
+            self.cursor += 1;
+        }
+    }
+
+    fn move_right(&mut self) {
+        if self.cursor >= self.input.len() {
+            return;
         }
 
         self.cursor += 1;
+    }
+
+    fn move_left(&mut self) {
+        self.cursor = self.cursor.saturating_sub(1);
+    }
+
+    fn delete_char(&mut self) {
+        if self.cursor == 0 {
+            return;
+        }
+
+        self.input.remove(self.cursor);
+
+        if self.cursor >= self.input.len() - 1 {
+            self.cursor = self.input.len() - 1;
+        }
+    }
+
+    fn backspace(&mut self) {
+        self.delete_char();
+        self.cursor = self.cursor.saturating_sub(1);
     }
 
     fn render(&self, frame: &mut Frame) {}
@@ -40,6 +69,22 @@ fn main() -> color_eyre::Result<()> {
 
                 KeyCode::Char(c) => {
                     app.insert_char(c);
+                }
+
+                KeyCode::Left => {
+                    app.move_left();
+                }
+
+                KeyCode::Right => {
+                    app.move_right();
+                }
+
+                KeyCode::Backspace => {
+                    app.backspace();
+                }
+
+                KeyCode::Delete => {
+                    app.delete_char();
                 }
 
                 _ => {}
